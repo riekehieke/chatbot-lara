a3e49ee366342f57348a52a800fe75fcd323728d
riekehieke <30436310+riekehieke@users.noreply.github.com>
2018-06-18 18:10:02 +0200

    page change function

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 document.addEventListener('init', function (event) {
   var page = event.target;
-});
-document.addEventListener("keyup", function () {
-  var regCode = document.querySelector("ons-input").value;
-  if (regCode === "1") {
-    document.querySelector('#nav').pushPage('pages/start.html', { data: { title: 'Page 2' } });
+
+  if (page.id === 'login') {
+    document.addEventListener("keyup", function () {
+      var regCode = document.querySelector("ons-input").value;
+      if (regCode === "1") {
+        document.querySelector('#nav').pushPage('pages/start.html', { data: { title: 'Start' } });
+      }
+    });
   }
+  if (page.id === 'start') {
+    page.querySelector('#unwohl-button').onclick = function () {
+      document.querySelector('#nav').pushPage('pages/unwohl.html', { data: { title: 'Unwohl' } });
+    };
+  }
+
 });
