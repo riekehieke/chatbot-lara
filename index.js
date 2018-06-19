74a59e2784d6a4a93826545832d5ad265d2d9cd6
riekehieke <30436310+riekehieke@users.noreply.github.com>
2018-06-19 12:19:16 +0200

    keyup fix

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
+function regCode() {
+  var regCode = document.getElementById("reg-code").value;
+  if (regCode === "321") {
+    document.querySelector('#nav').pushPage('pages/start.html', { data: { title: 'Start' } });
+  }
+}
+
 document.addEventListener('init', function (event) {
   var page = event.target;
 
-  if (page.id === 'login') {
-    document.addEventListener("keyup", function () {
-      var regCode = document.querySelector("ons-input").value;
-      if (regCode === "1") {
-        document.querySelector('#nav').pushPage('pages/start.html', { data: { title: 'Start' } });
-      }
-    });
-  }
+
   if (page.id === 'start') {
     page.querySelector('#unwohl-button').onclick = function () {
       document.querySelector('#nav').pushPage('pages/unwohl.html', { data: { title: 'Unwohl' } });
