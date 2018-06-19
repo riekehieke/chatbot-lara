30fdc8d5e9b35a5ac5d01e3145aea9dd4cf43f30
riekehieke <30436310+riekehieke@users.noreply.github.com>
2018-06-19 20:58:53 +0200

    symptom analyse kopfschmerzen

--- index.js
+++ index.js
@@ -34,7 +34,30 @@ document.addEventListener('init', function (event) {
 function Beschwerden() {
   var input = document.getElementById("beschwerden-input").value
   var bubble = document.getElementById('user-beschwerden')
-  bubble.innerHTML = "<p>" + input + "</p>"
+  bubble.innerHTML = "<p id='p-user-beschwerden'>" + input + "</p>"
   bubble.style.opacity = "1"
-  document.getElementById("beschwerden-input").value = ''
+  document.getElementById("beschwerden-input").value = ""
+  document.getElementById("beschwerden-input").style.opacity = "0"
+  document.getElementById("beschwerden-fab").style.opacity = "0"
+  //Antwort auf Input
+  setTimeout(function () {
+    var user = document.getElementById("p-user-beschwerden").innerHTML
+    var bubble = document.getElementById("bot-nachfrage")
+    var symptome1 = ["Kopfschmerzen", "hinter", "Augen"]
+    if (symptome1.some(el => user.includes(el))) {
+      bubble.innerHTML = "<p>Haben Sie außerdem eines oder mehrere dieser Symptome?</p>"
+      bubble.style.opacity = "1"
+    } else {
+      bubble.innerHTML = "<p>Ich kann Ihre Symptome leider nicht auswerten. Bitte versuchen Sie es erneut.</p>"
+      bubble.style.opacity = "1"
+      setTimeout(function () {
+        document.getElementById("user-beschwerden").innerHTML = ""
+        document.getElementById("user-beschwerden").style.opacity = "0"
+        document.getElementById("beschwerden-input").style.opacity = "1"
+        document.getElementById("beschwerden-fab").style.opacity = "1"
+        bubble.innerHTML = ""
+        bubble.style.opacity = "0"
+      }, 2000)
+    }
+  }, 1500)
 }
